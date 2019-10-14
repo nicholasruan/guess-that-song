1a26cbf54bfa018dba6f91d9218c20103bafe0cb
chrisyuaners <chrisyuaners@gmail.com>
2019-10-14 14:31:55 -0400

    created structure of game

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import LandingPage from "./components/LandingPage.js";
-import GameStart from "./components/GameStart.js";
+import GameContainer from "./containers/GameContainer.js";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
         <Switch>
           <Route exact path="/" component={LandingPage}/>
-          <Route path="/gamestart" component={GameStart}/>
+          <Route path="/gamestart" component={GameContainer}/>
         </Switch>
-        </BrowserRouter>
       </div>
     );
   }
